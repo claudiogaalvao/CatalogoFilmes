bc70cdd39a5f8fdbad80d811d787fed488cdf4ca
cvascong <claudiogaalvao@gmail.com>
2020-06-01 15:50:03 -0300

    Nova funcionalidade de Detalhes do filme

--- app/src/main/java/br/com/claudiogalvao/catalogofilmes/ui/adapter/ListaFilmesAdapter.kt
+++ app/src/main/java/br/com/claudiogalvao/catalogofilmes/ui/adapter/ListaFilmesAdapter.kt
@@ -1,16 +1,24 @@
 package br.com.claudiogalvao.catalogofilmes.ui.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Toast
 import br.com.claudiogalvao.catalogofilmes.R
 import br.com.claudiogalvao.catalogofilmes.model.Filme
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.filme_item.view.*
 
+interface OnClickListener {
+
+    fun onClick(filme: Filme)
+}
+
 class ListaFilmesAdapter(val context: Context, val filmes: ArrayList<Filme>) : BaseAdapter() {
+    var onClickListener: OnClickListener? = null
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         var filme = filmes[position]
@@ -19,6 +27,7 @@ class ListaFilmesAdapter(val context: Context, val filmes: ArrayList<Filme>) : B
         val filmeView = inflator.inflate(R.layout.filme_item, null)
 
         Picasso.get().load(filme.getCapa()).into(filmeView.filme_item_imageview)
+        filmeView.filme_item_imageview.setOnClickListener { onClickListener?.onClick(filme) }
 
         return filmeView
     }
